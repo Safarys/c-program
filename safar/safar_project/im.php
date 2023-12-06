e266922848b72570a034f8249b86e95ad6cb43f4
Safar <116191536+Safarys@users.noreply.github.com>
2023-12-06 11:15:38 +0530

    im.php

--- safar/safar_project/im.php
+++ safar/safar_project/im.php
@@ -53,10 +53,7 @@ $m=$_POST['mark'];
         </body>
         </html>
         
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+
 
 
 
