71074d9377e1c790e614f30c80cb665918cd5436
Safar <116191536+Safarys@users.noreply.github.com>
2024-01-29 13:03:09 +0530

    Delete count_alphabet.py

--- count_alphabet.py
+++
@@ -1,7 +0,0 @@
-nm=input("Enter the Names separated with commas ")
-nmlist=nm.split(",")
-c=0
-for name in nmlist:
-    if(name[0]=='A'):
-        c+=1
-print("Count start with alphabet 'A' is ",c)        
